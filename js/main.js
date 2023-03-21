652df2574641dd8b7f70f358114459080c221960
hcdante <hectorhugoandradelinares@gmail.com>
2023-03-21 13:06:32 -0600

    Se modificó el archivo JS para probar el uso de funciones.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+//let cont=0;
+//let nombre="Alberto";
+//let edad=29;
+//let x=20;
+
+function suma(a,b) {
+    return (a+b);
+}//suma
+
+function iniciales (nombre){
+    return nombre.toUpperCase();//tolowerCase()
+}//iniciales
+
+
 console.log("!Hola, Javascript externo!");
