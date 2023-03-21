bff10b70795c54593cec273d8c3d57c96f124b91
hcdante <hectorhugoandradelinares@gmail.com>
2023-03-21 15:41:29 -0600

    Se modificó el archivo JS para probar el uso de funciones y otros elementos.

--- js/main.js
+++ js/main.js
@@ -3,13 +3,26 @@
 //let edad=29;
 //let x=20;
 
-function suma(a,b) {
+let resultado = suma(10,35);
+console.log(resultado);
+
+//function suma(a,b) {
+//    return (a+b);
+//} //suma
+
+const suma= function (a,b){
     return (a+b);
 }//suma
 
-function iniciales (nombre){
-    return nombre.toUpperCase();//tolowerCase()
-}//iniciales
 
+function iniciales(nombre){
+    //return nombre.toUpperCase(); // Mayúsculas.
+    //return nombre.tolowerCase(); // Minúsculas.
+    return nombre.charAT(0).toUpperCase(); // Carácter en la posición 0.
+}//iniciales
+function currentYear(){
+    let fecha = new Date();
+    return fecha.getFullYear();     
+}//currentYear
 
 console.log("!Hola, Javascript externo!");
